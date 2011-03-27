7543377fb9b884bdabe0a37ebc2ebad6a482492e
Thilo Savage <thilosavage@gmail.com>
2011-03-27 18:56:24 +0200

    'test' :x

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $route = new route();
 $controller = factory::build($route);
 $controller->run($route);
 
-if(site::debug)log::error("The application finished without any errors.");
+if(site::debug)log::error("The application finished without any errors..");
 site::debug?log::set(mysql_error()):'';
 echo log::write();
 
